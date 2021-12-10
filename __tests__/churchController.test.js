56d0d56783742f2a09857afde793f6fac5110ce2
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-12-10 22:41:40 +0800

    Attempt db unit test for church controller

--- __tests__/churchController.test.js
+++ __tests__/churchController.test.js
@@ -29,16 +29,47 @@ describe("churchController", () => {
             responseStub.restore();
         });
 
-        it("returns an appropriate response", () => {
+        it("Should return a true response", () => {
             // Arrange
             expectedResult = true;
-            responseStub = sandbox.stub(db, "find").yields("", "", [], "", expectedResult);
+            responseStub = sandbox.stub(db, "find").yields(expectedResult);
+
+            // Act
+            churchController.getChurchInfo(req, res);
+
+            // Assert
+            sinon.assert.calledWith(res.send, JSON.stringify(true));
+        });
+
+        it("Should return a false response", () => {
+            // Arrange
+            expectedResult = false;
+            responseStub = sandbox.stub(db, "find").yields(expectedResult);
 
             // Act
             churchController.getChurchInfo(req, res);
 
             // Assert
             sinon.assert.calledWith(res.send, JSON.stringify(expectedResult));
+        });
+
+        it("Should return a true update response", () => {
+            req = {
+                body: {
+                    church_id: 1,
+                    address_id: 1
+                },
+            };
+
+            // Arrange
+            expectedResult = true;
+            responseStub = sandbox.stub(db, "update").yields(expectedResult);
+
+            // Act
+            churchController.putUpdateChurch(req, res);
+
+            // Assert
+            sinon.assert.calledWith(res.send, expectedResult);
         });
     });
 });
