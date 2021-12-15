89e1271fd3d9267323a41949b39a483f45268acb
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-12-15 21:32:35 +0800

    Create observation.test.js

---
+++ __tests__/observation.test.js
@@ -0,0 +1,102 @@
+const sinon = require('sinon');
+const request = require('supertest');
+const app = require('../app');
+const updateController = require('../controllers/observationController');
+const db = require("../models/db");
+
+describe('Update Controller', () => {
+    let person = {};
+    let res = {};
+
+    const sandbox = sinon.createSandbox();
+    beforeEach(() => {
+        req = {
+            body: {
+                observation_id: 1234,
+                observer: "Johan",
+                observee: "Nina",
+                comment: "Nicely Done"
+            }
+        };
+
+        res = {
+            send: sandbox.spy(),
+            render: sandbox.spy()
+        };
+    })
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("Should have access to postAddObservation", () => {
+        //Arrange        
+        sandbox.stub(db, "insert").yields([1234]);
+        expectedResult = {
+            observer: "Johan",
+            observee_id: "Nina",
+            comment: "Nicely Done",
+            date: new Date().toISOString(),
+            observation_id: 1234,
+            layout: false
+        };
+
+        //Act
+        updateController.postAddObservation(req, res);
+
+        //Assert
+        sinon.assert.calledWith(res.render, 'partials/observation', expectedResult);
+    });
+
+    it("Should have access to putUpdateObservation where the update is successful", () => {
+        //Arrange                
+        sandbox.stub(db, "update").yields([{
+            observer: "Johan",
+            observee_id: "Nina",
+            comment: "Nicely Done",
+        }]);
+        expectedResult = true;
+
+        //Act
+        updateController.putUpdateObservation(req, res);
+
+        //Assert
+        sinon.assert.calledWith(res.send, expectedResult);
+    });
+
+    it("Should have access to putUpdateObservation where the update is unsuccessful", () => {
+        //Arrange        
+        sandbox.stub(db, "update").yields(null);
+        expectedResult = false;
+
+        //Act
+        updateController.putUpdateObservation(req, res);
+
+        //Assert
+        sinon.assert.calledWith(res.send, expectedResult);
+    });
+
+    it("Should have access to delObservation where the delete is successful", () => {
+        //Arrange                
+        sandbox.stub(db, "delete").yields(req.body.observation_id);
+        expectedResult = true;
+
+        //Act
+        updateController.delObservation(req, res);
+
+        //Assert
+        sinon.assert.calledWith(res.send, expectedResult);
+    });
+
+    it("Should have access to delObservation where the delete is unsuccessful", () => {
+        //Arrange                
+        sandbox.stub(db, "delete").yields(null);
+        expectedResult = false;
+
+        //Act
+        updateController.delObservation(req, res);
+
+        //Assert
+        sinon.assert.calledWith(res.send, expectedResult);
+    });
+});
