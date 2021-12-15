dc85387595704a08a48532f28b39b3ff15dba0a5
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2021-12-15 18:16:01 +0800

    Added negative test case for updateNonMemberToNonMember

--- __tests__/update.test.js
+++ __tests__/update.test.js
@@ -9,22 +9,39 @@ describe('Update Controller', () => {
     let res = {};
 
     const sandbox = sinon.createSandbox();
-
-    afterEach(() => {
-        sandbox.restore();
-    });
-
-    it("Should have access to updateNonMemberToNonMember", () => {
-        //Arrange
+    beforeEach(() => {
         person = {
             firstName: "John",
             midName: "K",
             lastName: "Doe",
             personId: 123
         }
+    })
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("Should have access to updateNonMemberToNonMember where the result yields something", () => {
+        //Arrange        
         res.send = sandbox.spy();
         expectedResult = person.personId;
         sandbox.stub(db, "update").yields(person.personId);
+
+        //Act
+        updateController.updateNonMemberToNonMember(person, function (result) {
+            res.send(result);
+        });
+
+        //Assert
+        sinon.assert.calledWith(res.send, expectedResult);
+    });
+
+    it("Should have access to updateNonMemberToNonMember where the result yields nothing/null", () => {
+        //Arrange
+        res.send = sandbox.spy();
+        expectedResult = false;
+        sandbox.stub(db, "update").yields(null);
 
         //Act
         updateController.updateNonMemberToNonMember(person, function (result) {
