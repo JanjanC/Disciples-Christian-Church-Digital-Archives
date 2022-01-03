82a588102fa50b553139578e058c83c6a8751113
Josh Aaron Khyle S. Uson <josh_uson@dlsu.edu.ph>
2022-01-03 21:39:25 +0800

    Changed allowed edit prenup level access conditional from strictly level 3 to level 2 above
    Issue #9 Fix

--- controllers/prenupController.js
+++ controllers/prenupController.js
@@ -113,7 +113,7 @@ const prenupController = {
      * This function selects the member based on member_id and renders this
      * one member in the dropdown options in add-prenup-temp.hbs
      */
-    function selectMember (member) {
+    function selectMember(member) {
       // let brideNames = []
       // let groomNames = []
       const conditions3 = new Condition(queryTypes.where)
@@ -159,7 +159,7 @@ const prenupController = {
             }
           })
 
-        // if the member is a female
+          // if the member is a female
         } else {
           const brideNames = result
           // find all groom members
@@ -193,7 +193,7 @@ const prenupController = {
      * This function selects all the single members and renders all names
      * in the dropdown option in add-prenup-temp.hbs
      */
-    function selectAllMembers () {
+    function selectAllMembers() {
       const cond1 = new Condition(queryTypes.where)
       const cond2 = new Condition(queryTypes.whereNull)
       const cond3 = new Condition(queryTypes.where)
@@ -633,7 +633,7 @@ const prenupController = {
    */
   getEditPrenup: function (req, res) {
     const prenupId = req.params.prenup_id
-    if (parseInt(req.session.level) === 3 || parseInt(req.session.editId === parseInt(prenupId))) {
+    if (parseInt(req.session.level) > 1 || parseInt(req.session.editId === parseInt(prenupId))) {
       console.log(prenupId)
       const data = {
         scripts: ['editPrenup', 'edit'],
@@ -785,7 +785,7 @@ const prenupController = {
       updateNonMemberToNonMember(person, sendReply)
     }
 
-    function sendReply (result) {
+    function sendReply(result) {
       if (result) {
         res.send(JSON.stringify(result))
       } else {
@@ -828,7 +828,7 @@ const prenupController = {
       updateNonMemberToNonMember(person, sendReply)
     }
 
-    function sendReply (result) {
+    function sendReply(result) {
       if (result) {
         res.send(JSON.stringify(result))
       } else {
